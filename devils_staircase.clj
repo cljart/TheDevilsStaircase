dbee085093a650d9aac86477096a3954273ef3da
Arne Brasseur <arne@arnebrasseur.net>
2015-03-02 21:53:55 +0100

    The devils staircase, IN CLOJURE

--- devils_staircase.clj
+++ devils_staircase.clj
@@ -1,59 +1,34 @@
-(ns mad-sounds.devils_staircase2
+(ns mad-sounds.devils-staircase
  (:require [overtone.live :refer :all]
            [overtone.inst.sampled-piano :refer :all]))
 
-(sampled-piano 46)
-(sampled-piano 43)
-(sampled-piano 40)
 
-(defn play-chord [root chord-name]
-  (doseq [y (chord root chord-name)] (sampled-piano y)))
+(def metro (metronome 100))
 
-(let [time (now)]
-  (at time (play-chord :c4 :major))
-  (at (+ 300 time) (sampled-piano (note :c4)))
-  (at (+ 500 time) (sampled-piano (note :e4)))
-  (at (+ 1000 time) (sampled-piano (note :g4)))
+(defn audio-level [time finger amount-fingers]
+  (cond
+    (= finger 0) (/ time 11)
+    (= finger (dec amount-fingers)) (- 1 (/ time 11))
+    :else 1))
 
-  (at (+ 1500 time) (play-chord :a3 :minor))
+(def amount-fingers 5)
 
-  (at (+ 1800 time) (sampled-piano (note :a3)))
-  (at (+ 2000 time) (sampled-piano (note :c4)))
-  (at (+ 2500 time) (sampled-piano (note :e4)))
+(defn finger->note [finger time]
+  (+ 50 (* 12 finger) time))
 
-  (at (+ 3000 time) (play-chord :f3 :major))
+(defn play-finger [finger amount-fingers]
+  (loop [time 0]
 
-  (at (+ 3300 time) (sampled-piano (note :f3)))
-  (at (+ 3500 time) (sampled-piano (note :a3)))
-  (at (+ 4000 time) (sampled-piano (note :c4)))
+    (sampled-piano
+      (finger->note finger time)
+      (audio-level time finger amount-fingers))
 
-  (at (+ 4500 time) (play-chord :g3 :major))
+    (java.lang.Thread/sleep 500)
 
-  (at (+ 4800 time) (sampled-piano (note :g3)))
-  (at (+ 5000 time) (sampled-piano (note :b3)))
-  (at (+ 5500 time) (sampled-piano (note :d4)))
+    (if (< time 11)
+      (recur (inc time))
+      (recur 0))))
 
-
-  (at (+ 6000 time) (play-chord :c4 :major))
-  (at (+ 6300 time) (sampled-piano (note :c4)))
-  (at (+ 6500 time) (sampled-piano (note :e4)))
-  (at (+ 7000 time) (sampled-piano (note :g4)))
-
-  (at (+ 7500 time) (play-chord :a3 :minor))
-
-  (at (+ 7800 time) (sampled-piano (note :a3)))
-  (at (+ 8000 time) (sampled-piano (note :c4)))
-  (at (+ 8500 time) (sampled-piano (note :e4)))
-
-  (at (+ 9000 time) (play-chord :f3 :major))
-
-  (at (+ 9300 time) (sampled-piano (note :f3)))
-  (at (+ 9500 time) (sampled-piano (note :a3)))
-  (at (+ 10000 time) (sampled-piano (note :c4)))
-
-  (at (+ 10500 time) (play-chord :g3 :major))
-
-  (at (+ 10800 time) (sampled-piano (note :g3)))
-  (at (+ 11000 time) (sampled-piano (note :b3)))
-  (at (+ 11500 time) (sampled-piano (note :d4)))
-)
+(dotimes [n amount-fingers]
+  (future
+    (play-finger n amount-fingers)))
